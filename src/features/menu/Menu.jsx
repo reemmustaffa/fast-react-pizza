0df101a9f002efbaacf7bb859748ea9a739e404c
reem_mustafa <reemmustafa905@gmail.com>
2025-09-21 13:33:39 +0300

    fetching data with react router using loader

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 function Menu() {
-  return <h1>Menu</h1>;
+  //useLoaderData:بيوفرلك البيانات اللي loader رجعها للـ route الحالي.
+  const menu = useLoaderData();
+  console.log(menu);
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+//تحميل البيانات المتعلقة بالصفحة عند الدخول للـ route بتاعها
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
