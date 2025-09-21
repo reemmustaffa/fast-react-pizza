5dde0406ceea439b1661c56eee4188aa2ad2f6e5
reem_mustafa <reemmustafa905@gmail.com>
2025-09-21 12:04:51 +0300

    building app layout and making it layout router

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
+//used for data loading and data fetching
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/new", element: <CreateOrder /> },
-  { path: "/order/:orderId", element: <Order /> },
+  {
+    // layout routing
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
 ]);
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <h2>Reem</h2>
+    </header>
+  );
+}
+
+export default Header;
