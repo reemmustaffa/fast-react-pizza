500600a046de192deb122fbccfea19e1a2011c99
reem_mustafa <reemmustafa905@gmail.com>
2025-09-23 18:00:31 +0300

    styling menu and adding type to the button component to select if is primary or small

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,12 +1,11 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 function Menu() {
   //useLoaderData:بيوفرلك البيانات اللي loader رجعها للـ route الحالي.
   const menu = useLoaderData();
-  console.log(menu);
   return (
-    <ul>
+    <ul className='divide-y divide-stone-200 px-2'>
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
