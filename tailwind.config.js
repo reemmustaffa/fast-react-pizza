619791f377eb1f9f35df984fc760e172c95aada7
reem_mustafa <reemmustafa905@gmail.com>
2025-09-23 14:35:45 +0300

    configuration tailwind to update font.sans that is exists in the base tailwind to mono

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
-  // eslint-disable-next-line
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  // اللي بحطه هنا ببدل اللي موجود هناك واحطه بالقيمه دي وعشان هما ملهمش فونت فاملي فطبقه عادي
   theme: {
-    extend: {},
+    fontFamily: {
+      // انا هنا غيرت في الكونفجريشن بتاعت الخط سانس
+      sans: 'Roboto Mono,monospace',
+    },
+    // هنا بحط الحاجات البديله لو في خاصيه مش موجوده
+    extend: {
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
