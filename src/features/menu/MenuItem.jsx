82de025061379870bf2b95d7e0ac0d1b5ad27c14
reem_mustafa <reemmustafa905@gmail.com>
2025-09-24 09:43:22 +0300

    adding menu items to the cart and update button with onclick prop

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,21 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addItem } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
+  function handleAddTOCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 2,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className='flex gap-4 py-2'>
@@ -25,7 +38,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type='small'>Add to cart</Button>
+          {!soldOut && (
+            <Button onClick={handleAddTOCart} type='small'>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
