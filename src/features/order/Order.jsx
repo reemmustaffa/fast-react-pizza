63ade2daf055fa8da173f7de09434381740068ab
reem_mustafa <reemmustafa905@gmail.com>
2025-09-24 21:42:29 +0300

    fetching data from anthor loader (menu) using useFetcher without navigaton between pages

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,15 +1,27 @@
 // Test ID: IIDSAT
 import OrderItem from './OrderItem';
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from '../../utils/helpers';
+import { useEffect } from 'react';
 
 function Order() {
   const order = useLoaderData();
+  //عشان لو عايزه اجيب الداتا اللي في اي روت تاني
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
+
+  console.log(fetcher.data);
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -52,7 +64,15 @@ function Order() {
 
       <ul className='divide-y divide-stone-200 border-b border-t'>
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
